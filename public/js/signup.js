11cb16bdc64e42ef3db088517a16bdf859f5f63c
nguyendinhkhanhha296 <nguyendinhkhanhha296@gmail.com>
2022-09-27 21:42:59 -0700

    initial commit

--- public/js/signup.js
+++ public/js/signup.js
@@ -21,7 +21,7 @@ async function signupFormHandler(event) {
         // check the response status
         if (response.ok) {
             console.log('success');
-            document.location.replace('/cart');
+            document.location.replace('/');
         } else {
             alert(response.statusText);
         }
