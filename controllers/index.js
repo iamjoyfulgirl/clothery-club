dc41eace820925e2629f2359ee8fd284f584f59f
Abraham <abrahamsmith1998@gmail.com>
2022-09-27 22:34:22 -0400

    finally running

--- controllers/index.js
+++ controllers/index.js
@@ -2,5 +2,8 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
+router.get('/', function(req, res, next){
+    res.render('home', {});
+});
 
 module.exports = router;
