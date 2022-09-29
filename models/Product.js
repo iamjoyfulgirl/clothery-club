38aecede5dd9e0ce3eec283d44bfae944390140e
nguyendinhkhanhha296 <nguyendinhkhanhha296@gmail.com>
2022-09-29 15:10:56 -0700

    remove size and color

--- models/Product.js
+++ models/Product.js
@@ -45,10 +45,6 @@ Product.init({
     type: DataTypes.STRING,
     allowNull: false
   },
-  size: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
   cart_id: {
     type: DataTypes.INTEGER,
     references: {
